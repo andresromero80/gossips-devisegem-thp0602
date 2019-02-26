4abfcec2fbcb74da0b5ef5e89e50b7d1dad76891
andresromero80 <andresromero80@hotmail.com>
2019-02-26 15:01:34 +0100

    Working edit mission and categories filter

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -2,6 +2,7 @@ class MissionsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @hash = {"cancer" => "Cancers", "psyco" => "Compétences psychosociales", "addict" => "Conduites addictives", "environ" => "Environnement", "handicap" => "Handicap", "hepatites" => "Hépatites (VHB - VHC)", "chronic" => "Maladies chroniques (autres)", "infec" => "Maladies infectieuses (autres)", "medic" => "Médicament", "nutri" => "Nutrition (alimentation et activité physique)", "polsante" => "Politique de santé", "precar" => "Précarité", "parent" => "Périnatalité - Parentalité", "routier" => "Risque routier", "travail" => "Santé au travail", "dental" => "Santé bucco-dentaire", "mental" => "Santé mentale", "school" => "Santé scolaire", "violence" => "Violence (dont mutilations sexuelles)", "sexual" => "Sexualité (contraception, IVG)", "vih" => "VIH-Sida IST", "vaccin" => "Vaccination", "elder" => "Vieillissement"}
     if current_user
       @missions = Mission.where(user_id: current_user.id)
     else 
@@ -11,6 +12,7 @@ class MissionsController < ApplicationController
   end 
 
   def new
+    @hash = {"cancer" => "Cancers", "psyco" => "Compétences psychosociales", "addict" => "Conduites addictives", "environ" => "Environnement", "handicap" => "Handicap", "hepatites" => "Hépatites (VHB - VHC)", "chronic" => "Maladies chroniques (autres)", "infec" => "Maladies infectieuses (autres)", "medic" => "Médicament", "nutri" => "Nutrition (alimentation et activité physique)", "polsante" => "Politique de santé", "precar" => "Précarité", "parent" => "Périnatalité - Parentalité", "routier" => "Risque routier", "travail" => "Santé au travail", "dental" => "Santé bucco-dentaire", "mental" => "Santé mentale", "school" => "Santé scolaire", "violence" => "Violence (dont mutilations sexuelles)", "sexual" => "Sexualité (contraception, IVG)", "vih" => "VIH-Sida IST", "vaccin" => "Vaccination", "elder" => "Vieillissement"}
   	@mission = Mission.new()
   end
 
@@ -32,9 +34,29 @@ class MissionsController < ApplicationController
     @users = User.all
   end
 
+  def edit
+    @hash = {"cancer" => "Cancers", "psyco" => "Compétences psychosociales", "addict" => "Conduites addictives", "environ" => "Environnement", "handicap" => "Handicap", "hepatites" => "Hépatites (VHB - VHC)", "chronic" => "Maladies chroniques (autres)", "infec" => "Maladies infectieuses (autres)", "medic" => "Médicament", "nutri" => "Nutrition (alimentation et activité physique)", "polsante" => "Politique de santé", "precar" => "Précarité", "parent" => "Périnatalité - Parentalité", "routier" => "Risque routier", "travail" => "Santé au travail", "dental" => "Santé bucco-dentaire", "mental" => "Santé mentale", "school" => "Santé scolaire", "violence" => "Violence (dont mutilations sexuelles)", "sexual" => "Sexualité (contraception, IVG)", "vih" => "VIH-Sida IST", "vaccin" => "Vaccination", "elder" => "Vieillissement"}
+    # @user = User.find(params[:id])
+    if current_user.id == Mission.find(params[:id]).user_id
+      @mission = Mission.find(params[:id])
+    else 
+      flash[:warning] = "You're not able to edit other users"
+      redirect_to root_path
+    end 
+  end
+
+  def update
+    @mission = Mission.find(params[:id])
+    @mission.update(mission_params)
+    redirect_to missions_path
+  end
+
     private
 
   def mission_params
-    params.require(:mission).permit(:title, :description)
+    params.require(:mission).permit(:title, :theme, :program, :context, 
+      :objectif, :description, :partner, :start, :end, :duration, 
+      :frequence, :public, :numpeople, :type, :finance, :evaluation, 
+      :sector, :geolevel, :commune, :deplevel, :plan)
   end 
 end
